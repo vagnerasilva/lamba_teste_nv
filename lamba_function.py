06e88df53196029cb6eb4ed78864bf8b97f15b55
vagnerasilva <vagnerasilva>
2024-10-21 22:24:10 -0300

    feat: decode code

--- lamba_function.py
+++ lamba_function.py
@@ -2,10 +2,47 @@ from src.app import processor
 
 
 def lambda_handler(event, context):
+
+
+    #handler_token(event)
+
+
     response = processor.processor_lambda(event, context)
-
-
     return response
 
 
-print(lambda_handler({}, {}))
+event_mock = {
+    "body": "eyJ0ZXN0IjoiYm9keSJ9",
+    "resource": "/{proxy+}",
+    "path": "/path/to/resource",
+    "httpMethod": "POST",
+    "queryStringParameters": {
+        "foo": "bar"
+    },
+    "multiValueQueryStringParameters": {
+        "foo": ["bar"]
+        },
+    "headers": {
+        "Accept": "text",
+        "Accept-Encoding": "gzip, deflate, sdch",
+        "Accept-Language": "en-US,en;q=0.8",
+        "Cache-Control": "max-age=0",
+        "CloudFront-Forwarded-Proto": "https",
+        "CloudFront-Is-Desktop-Viewer": "true",
+        "CloudFront-Is-Mobile-Viewer": "false",
+        "CloudFront-Is-SmartTV-Viewer": "false",
+        "CloudFront-Is-Tablet-Viewer": "false",
+        "CloudFront-Viewer-Country": "US",
+        "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Custom User Agent String",
+        "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net)",
+        "X-Amz-Cf-Id": "cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA==",
+        "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+        "X-Forwarded-Port": "443",
+        "X-Forwarded-Proto": "https"
+    }
+
+}
+
+print(lambda_handler(event_mock, {}))
